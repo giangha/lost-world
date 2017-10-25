099498994597d463ae920572f42b69bd172e930a
giangha <gi4ngh495@gmail.com>
2017-10-25 00:46:20 -0500

    add animation for walking and direction facing

--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -8,12 +8,20 @@ public class player : MonoBehaviour {
 
     public bool grounded;
     private Rigidbody2D rb2d;
+    private Animator anim;
 	void Start () {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
 	}
      void Update()
-    {
-        
+    {       
+        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+        // direction facing
+        if(Input.GetAxis("Horizontal")< -0.1f)
+        { transform.localScale = new Vector3(-1, 1, 1); }
+
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        { transform.localScale = new Vector3(1, 1, 1); }
     }
     // Update is called once per frame
     void FixedUpdate () {
