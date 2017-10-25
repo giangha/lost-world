7f3ff8c2847cefc63fd7dc5d43f902c4f8b7b9f3
giangha <gi4ngh495@gmail.com>
2017-10-25 02:11:29 -0500

    add atking motion

--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class player : MonoBehaviour {
     public float speed = 50f;
     public float jumpPower = 150f;
-
+    public float maxspeed = 3;
+  //  public float jumpPower = 150f;
     public bool grounded;
     private Rigidbody2D rb2d;
     private Animator anim;
@@ -15,18 +16,27 @@ public class player : MonoBehaviour {
 	}
      void Update()
     {       
-        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+        anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
         // direction facing
         if(Input.GetAxis("Horizontal")< -0.1f)
         { transform.localScale = new Vector3(-1, 1, 1); }
 
         if (Input.GetAxis("Horizontal") > 0.1f)
         { transform.localScale = new Vector3(1, 1, 1); }
+
+        if(Input.GetButtonDown("Jump"))
+        { rb2d.AddForce(Vector2.up * jumpPower); }
     }
     // Update is called once per frame
     void FixedUpdate () {
         float movementHorizontal = Input.GetAxis("Horizontal");
        
         rb2d.AddForce((Vector2.right * speed)*movementHorizontal);
-	}
+
+        if(rb2d.velocity.x > maxspeed)
+        { rb2d.velocity = new Vector2(maxspeed, rb2d.velocity.y); }
+
+        if (rb2d.velocity.x < -maxspeed)
+        { rb2d.velocity = new Vector2(-maxspeed, rb2d.velocity.y); }
+    }
 }
---
+++ Assets/script/playeratk.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playeratk : MonoBehaviour
+{
+
+    private bool attacking = false;
+    public Collider2D atkTrigger;
+    private Animator amin;
+    private float attackTimer = 0;
+    public float attackCd = .3f;
+
+    void Awake()
+    {
+        amin = gameObject.GetComponent<Animator>();
+        atkTrigger.enabled = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("z") && !attacking)
+        {
+            attacking = true;
+            attackTimer = attackCd;
+            atkTrigger.enabled = true;
+        }
+
+        if(attacking)
+        { if(attackTimer>0)
+            { attackTimer -= Time.deltaTime; }
+            else
+            { attacking = false;
+                atkTrigger.enabled = false;
+            }
+        }
+        amin.SetBool("atking", attacking);
+    }
+}
