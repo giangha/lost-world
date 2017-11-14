227c68fe110406893f89f6e497e23a16aa4a6298
Melvin531 <33071218+Melvin531@users.noreply.github.com>
2017-11-14 02:27:32 -0600

    Mummy Updates

---
+++ Assets/script/Mummy.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mummy : MonoBehaviour {
+
+	public float speed = 10f;
+	Animator mummyAnimator;
+	public int currentHealth = 4;
+	public Slider mummySlider;
+
+	//facing
+	public GameObject mummyGraphic;
+	bool canFlip = true;
+	bool facingRight = false;
+	float flipTime = 3f;
+	float nextFlipChance = 0f;
+
+	//attacking
+	public float chargeTime;
+	float startChargingTime;
+	bool attacking;
+	Rigidbody2D mummyRB;
+
+	// Use this for initialization
+	void Start() {
+
+		mummyAnimator = GetComponent<Animator>();
+		mummyRB = GetComponent<Rigidbody2D>();
+		mummyGraphic = this.gameObject;
+
+		mummySlider.maxValue = currentHealth;
+		mummySlider.value = currentHealth;
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+		if (Time.time > nextFlipChance) {
+			if (Random.Range(0, 10) >= 2)
+				flipFacing();
+			nextFlipChance = Time.time + flipTime;
+		}
+
+		mummyAnimator.SetFloat ("speed", Mathf.Abs (mummyRB.velocity.x));
+
+		if(currentHealth <=0)
+		{ Destroy(gameObject); }
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if (other.tag == "Player") {
+			if (facingRight && other.transform.position.x < transform.position.x)
+				flipFacing ();
+			else if (!facingRight && other.transform.position.x > transform.position.x)
+				flipFacing ();
+
+			canFlip = false;
+			attacking = true;
+
+			startChargingTime = Time.time + chargeTime;
+		} 
+
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+
+		if (other.tag == "Player") {
+			if (startChargingTime <= Time.time) {
+
+				//attack behavior
+
+				if (!facingRight) {
+					if (Mathf.Abs (other.transform.position.x - this.transform.position.x) > 10.0f) {
+						mummyRB.velocity = new Vector2 (-1, 0) * speed;
+					} 
+
+					else {
+						mummyRB.velocity = new Vector2 (-1, 0.25f)*speed;
+						mummyAnimator.SetBool ("atk", true);
+					}
+				}
+				else {
+					if (Mathf.Abs (other.transform.position.x - this.transform.position.x) > 10.0f) {
+						mummyRB.velocity = new Vector2 (1, 0) * speed;
+					} 
+
+					else {
+						mummyRB.velocity = new Vector2 (1, 0.25f)*speed;
+						mummyAnimator.SetBool ("atk", true);
+					}
+				}
+
+				mummyAnimator.SetFloat("speed", Mathf.Abs(mummyRB.velocity.x));  //change animation
+
+			}
+		}
+
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+
+		if (other.tag == "Player") {
+			canFlip = true;
+			attacking = false;
+			mummyRB.AddForce(new Vector2(0f, 0f));
+			mummyAnimator.SetFloat("speed", Mathf.Abs(mummyRB.velocity.x));  //change animation
+			mummyAnimator.SetBool("atk", attacking);
+		}
+
+	}
+
+	void flipFacing() {
+
+		if (!canFlip)
+			return;
+		float facingX = transform.localScale.x;
+		facingX *= -1f;
+		transform.localScale = new Vector3(facingX, mummyGraphic.transform.localScale.y, mummyGraphic.transform.localScale.z);
+		facingRight = !facingRight;
+
+	}
+
+	public void Damage(int dmg)
+	{ 
+		mummySlider.gameObject.SetActive (true);
+		currentHealth -= dmg;
+		mummySlider.value = currentHealth;
+	}
+
+}
